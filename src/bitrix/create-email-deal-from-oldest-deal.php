e0fe1849d33f1da216268d15d51b0e5f3c5f421c
Emil Aliev <eo.aliev@bizprofi.ru>
2017-11-10 16:37:53 +0300

    Add script insert deal from email activities

---
+++ src/bitrix/create-email-deal-from-oldest-deal.php
@@ -0,0 +1,46 @@
+<?php
+
+use Bitrix\Main\Application;
+use Bitrix\Main\Config;
+use Bitrix\Main\Db;
+use Bitrix\Main\Loader;
+use Magnifico\Site\Model;
+
+if (!Loader::includeModule('crm')) {
+    var_dump('Not loaded crm');
+    return;
+}
+
+if (!Loader::includeModule('magnifico.site')) {
+    var_dump('Not loaded magnifico.site');
+    return;
+}
+
+$connection = Application::getInstance()->getConnection();
+
+$emailWheres = [];
+foreach (json_decode(Config\Option::get('magnifico.site', 'emailtracker_froms', '[]'), true) as $from) {
+    $emailWheres[] = new Db\SqlExpression("`a`.`SETTINGS` LIKE CONCAT('%', ?s '%')", $from);
+}
+
+$rows = $connection->query(new Db\SqlExpression(
+    'SELECT `a`.*
+    FROM `b_crm_act` `a`
+    WHERE
+        `a`.`TYPE_ID`=?i
+        AND `a`.`DIRECTION`=?i'.
+        (count($emailWheres) > 0 ? ' AND ('.implode(' OR ', $emailWheres).')': '').'
+        ORDER BY `a`.`ID` DESC',
+    \CCrmActivityType::Email,
+    \CCrmActivityDirection::Incoming
+));
+
+if ($rows->getSelectedRowsCount() <= 0) {
+    var_dump('Activities not found');
+    return;
+}
+
+while ($row = $rows->fetch()) {
+    $activity = Model\Activity::create($row);
+    var_dump($activity->createEmailDeal());
+}
