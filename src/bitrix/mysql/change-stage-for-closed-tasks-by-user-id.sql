2b043eaf06417b356f9ba9acbb2ac0ef01f2a29b
Emil Aliev <eo.aliev@bizprofi.ru>
2017-12-04 13:48:02 +0300

    Fix update task stage id

--- src/bitrix/mysql/change-stage-for-closed-tasks-by-user-id.sql
+++ src/bitrix/mysql/change-stage-for-closed-tasks-by-user-id.sql
@@ -1,6 +1,9 @@
 UPDATE `b_tasks_task_stage` `ts`
-INNER JOIN `b_tasks_task` `t` ON `t`.`ID`=`ts`.`TASK_ID`
+INNER JOIN `b_tasks` `t` ON `t`.`ID`=`ts`.`TASK_ID`
+INNER JOIN `b_tasks_stages` `s` ON `s`.`ID`=`ts`.`STAGE_ID`
 SET `ts`.`STAGE_ID`=119
 WHERE
-    `t`.`STATUS`=2,
-    `t`.`CREATED_BY`=17
+    `t`.`STATUS` IN (2, 4, 5) AND
+    `t`.`RESPONSIBLE_ID`=17 AND
+    `s`.`ENTITY_TYPE`='U' AND
+    `s`.`ENTITY_ID`=17
